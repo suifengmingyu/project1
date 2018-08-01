2d0f803c1b7147a4bf81fc81b26266b8980c6824
suifengmingyu <1014019059@qq.com>
2018-08-01 16:33:59 +0800

    实现城市搜索功能

--- 旅游网站/src/pages/city/City.vue
+++ 旅游网站/src/pages/city/City.vue
@@ -2,7 +2,7 @@
 <template>
 	<div class="city">
 		<city-header></city-header>
-		<city-search></city-search>
+		<city-search :cities="cities"></city-search>
 		<city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
 		<city-alphabet :cities="cities" @change="handelLeterChange"></city-alphabet>
 	</div>
