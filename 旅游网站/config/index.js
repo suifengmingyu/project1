1cdf6a21010c52bc05980250e78c6bd34b99ed46
suifengmingyu <1014019059@qq.com>
2018-08-03 10:05:03 +0800

    项目打包

--- 旅游网站/config/index.js
+++ 旅游网站/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist',//打包项目的访问路径
 
     /**
      * Source Maps
